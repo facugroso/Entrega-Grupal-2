811675710857ffcc74328afa685dba95ebb210dd
romicalleja <romycalleja.12.08@gmail.com>
2022-08-24 18:47:52 -0300

    ergerg

--- js/almacenar.js
+++
@@ -1,8 +0,0 @@
-const agregar = document.getElementById("agregar");
-const limpiar = document.getElementById("limpiar");
-const contenedor = document.getElementById("contenedor");
-const item = document.getElementById("item");
-
-agregar.addEventListener("click", () => {
-  item=localStorage.setItem();
-});
