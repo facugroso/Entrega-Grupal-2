2e4afd57602bcbbca27fda9369bf439c01b2fb85
romicalleja <romycalleja.12.08@gmail.com>
2022-08-24 18:46:52 -0300

    ergerg

--- js/almacenar.js
+++ js/almacenar.js
@@ -1,3 +1,8 @@
-const agregar = document.getElementById("agregar")
-const limpiar = document.getElementById("limpiar")
-const contenedor = document.getElementById("contenedor")
+const agregar = document.getElementById("agregar");
+const limpiar = document.getElementById("limpiar");
+const contenedor = document.getElementById("contenedor");
+const item = document.getElementById("item");
+
+agregar.addEventListener("click", () => {
+  item=localStorage.setItem();
+});
